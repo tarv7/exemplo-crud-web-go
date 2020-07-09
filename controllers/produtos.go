77645bdafde7f28358e1dbbbb224901121ef4744
tarv7 <thales.ramos@enjoei.com.br>
2020-07-09 17:33:39 -0300

    Fluxo de criação de novos produtos

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+	"strconv"
 	"web-go/models"
 )
 
@@ -11,4 +13,30 @@ var temp = template.Must(template.ParseGlob("templates/*.html"))
 func Index(w http.ResponseWriter, r *http.Request) {
 	todosOsProdutos := models.BuscaTodosOsProdutos()
 	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoConvertidoParaFloat, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversão do preço:", err)
+		}
+
+		quantidadeConvertidaParaInt, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversão da quantidade:", err)
+		}
+
+		models.CriarNovoProduto(nome, descricao, precoConvertidoParaFloat, quantidadeConvertidaParaInt)
+	}
+	http.Redirect(w, r, "/", 301)
 }
