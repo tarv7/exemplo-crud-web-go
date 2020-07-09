6b9c341dde00de82a23658a8e1ff4b0fb34256c3
tarv7 <thales.ramos@enjoei.com.br>
2020-07-09 17:45:14 -0300

    Deletando um produto

--- models/produtos.go
+++ models/produtos.go
@@ -30,6 +30,7 @@ func BuscaTodosOsProdutos() []Produto {
 			panic(err.Error())
 		}
 
+		p.Id = id
 		p.Nome = nome
 		p.Descricao = descricao
 		p.Preco = preco
@@ -53,4 +54,17 @@ func CriarNovoProduto(nome, descricao string, preco float64, quantidade int) {
 	insereDadosNoBanco.Exec(nome, descricao, preco, quantidade)
 
 	db.Close()
+}
+
+func DeletaProduto(id string) {
+	db := db.ConectaComBancoDeDados()
+
+	deletarOProduto, err := db.Prepare("delete from produtos where id=$1")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	deletarOProduto.Exec(id)
+
+	defer db.Close()
 }
